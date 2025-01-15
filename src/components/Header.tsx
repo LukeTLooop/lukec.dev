49f3bd2a3783888bc9f10a64a44743060aa58672
LukeTLooop <lukey@lukec.dev>
2025-01-15 11:16:24 -0700

    commit before switch from react-simple-typewriter to typewriter-effect

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,18 +7,24 @@ import "../styles/AnimatedText.css"
 function Header() {
     const [cursorBlink, setCursorBlink] = useState<boolean>(false);
     const [typeEffect] = useTypewriter({
-        words: ["Luke Christopherson"],
+        words: ["Luke Christopherson", "test", "i like pineapples", "actually, i like kiwis more tbh"],
         typeSpeed: 75,
         deleteSpeed: 40,
         onLoopDone: () => {
             setCursorBlink(true);
         },
+        onDelay: () => {
+            setCursorBlink(true);
+        },
+        onDelete: () => {
+            setCursorBlink(false);
+        }
     });
 
     return (
         <h1 className="text-center w-3/4 text-3xl text-green-500 animated-text">
             {typeEffect}
-            <Cursor cursorBlinking={cursorBlink} cursorColor="inherit" />
+            <Cursor cursorBlinking={cursorBlink} cursorStyle="_" cursorColor="inherit" />
         </h1>
     );
 }
